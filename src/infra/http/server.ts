24c27dda31bac00a8a262f32654f427b5baeef9b
moraiskaua <moraiskaua03@gmail.com>
2025-01-14 19:56:03 -0300

    feat: create validators and serializers compilers

--- src/infra/http/server.ts
+++ src/infra/http/server.ts
@@ -1,7 +1,30 @@
 import { fastifyCors } from '@fastify/cors'
 import { fastify } from 'fastify'
+import {
+  hasZodFastifySchemaValidationErrors,
+  serializerCompiler,
+  validatorCompiler,
+} from 'fastify-type-provider-zod'
 
 const server = fastify()
+
+server.setValidatorCompiler(validatorCompiler)
+server.setSerializerCompiler(serializerCompiler)
+
+server.setErrorHandler((error, request, reply) => {
+  if (hasZodFastifySchemaValidationErrors(error)) {
+    return reply.status(400).send({
+      message: 'Validation error',
+      errors: error.validation,
+    })
+  }
+
+  // TODO: implement a better error handler (Sentry/Datadog...)
+  console.error(error)
+  return reply.status(500).send({
+    message: 'Internal server error',
+  })
+})
 
 server.register(fastifyCors, {
   origin: '*',
