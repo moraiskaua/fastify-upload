4c9c132d985cfb3d2117c80fa443c25f73c4de9a
moraiskaua <moraiskaua03@gmail.com>
2025-01-14 20:40:43 -0300

    feat: config drizzle orm

--- src/env.ts
+++ src/env.ts
@@ -3,7 +3,7 @@ import { z } from 'zod';
 const envSchema = z.object({
   PORT: z.coerce.number().default(8080),
   NODE_ENV: z.enum(['development', 'test', 'production']).default('production'),
-  DATABASE_URL: z.string().url().startsWith('postgres://'),
+  DATABASE_URL: z.string().url().startsWith('postgresql://'),
 });
 
 export const env = envSchema.parse(process.env);
